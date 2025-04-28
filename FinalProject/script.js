2e893e200212018ea1edc01e5300d6cfb23f9cd9
MaxLenik <ml242097@umconnect.umt.edu>
2025-04-28 00:25:47 -0600

    finalupdate1

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -1,21 +1,42 @@
-var song
+let song;
 
 function preload() {
-    song = loadSound('challengersmatchpoint.mp3')
+  song = loadSound('challengersmatchpoint.mp3',
+    () => console.log('Sound loaded successfully'),
+    (err) => console.error('Sound failed to load', err)
+  );
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
+fft = new p5.FFT()
 }
 
 function draw() {
-    background(0,0,0)
+  background(0);
+  stroke(255)
+  noFill()
+
+  var wave = fft.waveform()
+
+  beginShape()
+  for (var i = 0; i <width; i++) {
+    var index = floor(map(i, 0, width, 0, wave.length))
+
+    var x = i
+    var y = wave[index] * 300 + height / 2
+    vertex(x,y)
+  }
+  endShape()
 }
 
 function mouseClicked() {
-    if (song.isPlaying()) {
-        song.pause()
-    } else {
-        song.play()
-    }
+  userStartAudio();
+  if (song.isPlaying()) {
+    song.pause();
+    noLoop()
+  } else {
+    song.play();
+    loop()
+  }
 }
